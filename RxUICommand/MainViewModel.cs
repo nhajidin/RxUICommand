b96bed93e58afbe788d0081cc5e8dc875d0d9ceb
Nazim Hajidin <nhajidin@gmail.com>
2016-12-29 22:40:20 -0500

    Add code

---
+++ RxUICommand/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RxUICommand
+{
+    public class MainViewModel : ReactiveObject
+    {
+        public MainViewModel()
+        {
+            count = this.WhenAnyObservable(x => x.DoIt)
+                .Scan(0, (acc, _) => acc + 1)
+                .StartWith(0)
+                .ToProperty(this, x => x.Count);
+
+            DoIt = ReactiveCommand.Create();
+        }
+
+        public ReactiveCommand<object> DoIt { get; protected set; }
+
+        ObservableAsPropertyHelper<int> count;
+        public int Count
+        {
+            get { return count.Value; }
+        }
+    }
+}
--- RxUICommand/MainWindow.xaml.cs
+++ RxUICommand/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ReactiveUI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,11 +19,32 @@ namespace RxUICommand
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, IViewFor<MainViewModel>
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            this.BindCommand(ViewModel, x => x.DoIt, x => x.doIt);
+
+            this.OneWayBind(ViewModel, vm => vm.Count, v => v.count.Text);
+
+            ViewModel = new MainViewModel();
         }
+
+        public MainViewModel ViewModel
+        {
+            get { return (MainViewModel)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+
+        object IViewFor.ViewModel
+        {
+            get { return ViewModel; }
+            set { ViewModel = (MainViewModel)value; }
+        }
+
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register("ViewModel", typeof(MainViewModel), typeof(MainWindow), new PropertyMetadata(null));
     }
 }
